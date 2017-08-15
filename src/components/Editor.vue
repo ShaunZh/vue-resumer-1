d38f3cbf3776d9d898ffb2e96d69e01aba33d8a7
hexonZhang <308826842@qq.com>
2017-08-15 20:51:48 +0800

    将个人经历重构为一个组件 ProfileEditor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -13,23 +13,102 @@
         </ol>
     </nav>
     <ol class="pannes">
-      <li v-for="i in [0, 1, 2, 3, 4, 5]"
+      <li v-bind:class="{active: currentTab === 0}">
+
+        <ProfileEditor v-bind:profile="profile" />
+
+        <!-- <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+          <el-form-item label="名称">
+            <el-input v-model="formLabelAlign.name"></el-input>
+          </el-form-item>
+          <el-form-item label="活动区域">
+            <el-input v-model="formLabelAlign.region"></el-input>
+          </el-form-item>
+          <el-form-item label="活动形式">
+            <el-input v-model="formLabelAlign.type"></el-input>
+          </el-form-item>
+        </el-form> -->
+      </li>
+
+      <li v-bind:class="{active: currentTab === 1}">
+        <h2>学习经历</h2>
+        <el-form>
+          <!-- 这个(work, index) 是vue定义的语法 -->
+          <div class="container" v-for="(work, index) in workHistory">
+            <el-form-item label="公司">
+              <el-input v-model="work.company"></el-input>
+            </el-form-item>
+
+            <el-form-item label="工作内容">
+              <el-input v-model="work.content"></el-input>
+            </el-form-item>
+            <!-- 这里传的是一个removeWorkHistory(index)函数调用，而在下面的addWorkHistory 传的是函数名，这是Vue自己支持的两种调用方法-->
+            <i class="el-icon-circle-close" v-on:click="removeWorkHistory(index)"></i>
+          </div>
+          <hr>
+          <el-button type="primary" v-on:click="addWorkHistory()">添加一项</el-button>
+        </el-form>
+      </li>
+
+      <li v-bind:class="{active: currentTab === 2}">
+        <h2>工作经历</h2>
+      </li>
+      <li v-bind:class="{active: currentTab === 3}">
+        <h2>项目经历</h2>
+      </li>
+      <li v-bind:class="{active: currentTab === 4}">
+        <h2>爱好</h2>
+      </li>
+      <li v-bind:class="{active: currentTab === 5}">
+        <h2>不知道</h2>
+      </li>
+
+      <!-- <li v-for="i in [0, 1, 2, 3, 4, 5]"
           v-bind:class="{active: currentTab === i}"
           >
           Tab {{i + 1}}
-      </li>
+      </li> -->
     </ol>
   </div>
 </template>
 
 <script>
+import ProfileEditor from './ProfileEditor'
   export default {
+    components: { ProfileEditor },
     data() {
       return {
         currentTab: 0,
-        icons: ['icon-ionc--', 'icon-phone', 'icon-work', 'icon-project', 'icon-book', 'icon-iocn_hobby']
+        icons: ['icon-ionc--', 'icon-phone', 'icon-work', 'icon-project', 'icon-book', 'icon-iocn_hobby'],
+        profile: {
+          name: '',
+          city: '',
+          birth: ''
+        },
+        workHistory: [
+          {company: '', content: ''},
+        ]
+      }
+    },
+    // created()函数是组件被创建之后的回调函数
+    created() {
+
+    },
+
+    methods: {
+      addWorkHistory() {
+        this.workHistory.push({
+          company: '', content: ''
+        })
+      },
+      removeWorkHistory(index) {
+        this.workHistory.splice(index, 1)
       }
     }
+
+
+
+
   }
 </script>
 
@@ -59,8 +138,20 @@
       }
     }
     > .pannes {
+      flex: 1;
+      .container {
+        position: relative;
+        .el-icon-circle-close{
+          position: absolute;
+          right: 0;
+          top: 0;
+        }
+      }
       > li {
+        height: 100%;
+        overflow: auto;
         display: none;
+        padding: 32px;
         &.active {
           display: block;
         }
---
+++ src/components/ProfileEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>个人信息</h2>
+    <el-form>
+      <el-form-item label="姓名">
+          <el-input v-model="profile.name"></el-input>
+      </el-form-item>
+      <el-form-item label="城市">
+          <el-input v-model="profile.city"></el-input>
+      </el-form-item>
+      <el-form-item label="出生年月">
+          <el-input v-model="profile.birth"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    // 这句话表示：我允许从外面接受一个名为profile的变量
+    // 就是别的组件向本组件传递数据
+    props: ['profile']
+  }
+</script>
