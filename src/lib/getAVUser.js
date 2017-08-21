21a64f6423f09be1c73c9e540bb14fe39ec8b537
hexonZhang <308826842@qq.com>
2017-08-21 14:17:12 +0800

    1. show error message 2. 因为Vue不能检测到对象属性的添加或删除，所以确保getAVUser的id和username的值不为undefined或null

--- src/lib/getAVUser.js
+++ src/lib/getAVUser.js
@@ -9,5 +9,7 @@ import AV from '../lib/leancloud'
 
 export default function (user) {
   var {id, attributes: {username}} = user || AV.User.current() || {attributes:{}};
-  return {id,  username};
+  return {
+    id: id || '',
+    username: username || ''};
 }
